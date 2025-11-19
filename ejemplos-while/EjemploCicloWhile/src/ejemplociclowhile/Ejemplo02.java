17f64b9de7481dd701248fb5eb29d90f3567f070
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-11-19 11:56:18 -0500

    Explicacion ejemplo 2

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -4,13 +4,16 @@
  * and open the template in the editor.
  */
 package ejemplociclowhile;
+
 import java.util.Locale;
 import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejemplo02 {
+
     /**
      * @param args the command line arguments
      */
@@ -20,17 +23,20 @@ public class Ejemplo02 {
         entrada.useLocale(Locale.US);
         int limite;
         int contador = 1;
-        System.out.println("Ingrese el Límite");
-        limite = entrada.nextInt(); 
-        while (contador <= limite){
-            
-            if (contador%2!=0) {
-                System.out.printf("Contador %d\n", 
-                    contador);
+        System.out.println("Ingrese el Límite"); // 6
+        limite = entrada.nextInt();
+        while (contador <= limite) {
+            if (contador % 2 != 0) {
+                System.out.printf("Contador %d\n",
+                        contador);
             }
-            
             contador = contador + 1;
         }
-        
     }
 }
+/* El programa ejecuta un ciclo repetitivo bajo la condicion impuesta dentro
+del mismo que dicta qe el valor que se ingrese como limite hara que el programa
+presente todos los valores que existan UNICAMENTE IMPARES que lleguen o se 
+acerquen a ese limite, esto sucede por que la condicion del if toma solo los
+valores cuyo modulo es 1, los cuales son justamente los IMPARES.
+*/
---
+++ ejemplos-while/EjemploCicloWhile/src/paqueteclases/Ejemplo01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqueteclases;
+
+/**
+ *
+ * @author jdavi
+ */
+public class Ejemplo01 {
+
+    public static void main(String[] args) {
+
+        int contador = 1;
+        int limite = 10;
+        
+        while (contador <= limite) {
+            System.out.printf("%d\n", contador);
+            contador = contador + 1;
+        }
+    }
+}
+
